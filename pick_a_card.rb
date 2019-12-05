4bed13d8728b3fe8bd8393d8b9ba385ea2874ca1
Katrin Drozhak <k.chubova@gmail.com>
2019-12-05 15:47:33 +0300

    Change word to image

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,6 @@
 values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-suits = %w(Diamonds Hearts Clubs Spades) # Буби, Черви, Крести, Пики
+
+suits = ['♦', '♠', '♥ ', '♣']
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
 puts "#{values.sample} of #{suits.sample}"
